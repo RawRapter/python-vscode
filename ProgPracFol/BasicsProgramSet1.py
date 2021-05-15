a9f928d1ce1989adeb78dc5e85959ec7657306dd
Anant Arun <anant.arun92@gmail.com>
2021-05-15 19:53:15 +0530

    Set 1 Prog add

--- ProgPracFol/BasicsProgramSet1.py
+++ ProgPracFol/BasicsProgramSet1.py
@@ -1,5 +1,5 @@
 """
-Basic Factorial Program using function and recursion
+1) Basic Factorial Program using function and recursion
 """
 def factorial(x):
     f = 1
@@ -17,14 +17,14 @@ x = 5
 print("Factorial of",x,"is: ",factorial(x))
 
 """
-Basic Program to make right angle triangle using * of 5
+2) Basic Program to make right angle triangle using * of 5
 """
 z = 5
 #z = int(input("")) #Uncomment this to take input, and comment above line
 for j in range(1,z+1):
     print("*"*j)
 """
-Basic Program Equilateral triangle
+3) Basic Program Equilateral triangle
 """
 y = 5
 y1 = 5
@@ -32,3 +32,44 @@ for k in range(1,y+1):
     print(" "*y1,end="")
     print("* "*k)
     y1 -= 1
+
+"""
+4) Basic Program to count number of digit in an integer
+"""
+g = 100
+#g = int(input("")) #Uncomment this to take input, and comment above line
+def count(g):
+    c = 0
+    while g!=0 :
+        g //= 10
+        c +=1
+    return c
+print(count(g))
+
+"""
+5) Basic program to check if number is Armstrong
+"""
+import math
+num1 = 371
+def armstrong(num):
+    n = 0
+    result = 0
+    copynum = num
+    n = len(str(num))
+    # while copynum != 0: #same work as above line
+    #     copynum /= copynum
+    #     n += 1
+    # copynum = num
+    #print("Value of N",n)
+    while copynum != 0:
+        rem = copynum%10
+        result += pow(rem,n)
+        copynum //= 10
+    #print("Value of result ",result)
+    if result == num:
+        return "Number {} is armstrong".format(num)
+    else:
+        return "Number {} is not armstrong".format(num)
+
+x = armstrong(num1)
+print(x)
